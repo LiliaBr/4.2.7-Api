e4437007630cb7b5584272f9691c30ea6da7b790
LiliaBr <94970798+LiliaBr@users.noreply.github.com>
2022-07-26 13:54:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -16,11 +16,7 @@ function init() {
             const options = { 
                 headers: {
                     'Content-Type': 'application/vnd.github+json',
-<<<<<<< HEAD
                     Authorization: 'Bearer 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',
-=======
-                    Authorization: 'Bearer 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',
->>>>>>> a6507cd868b464d030fbd3ec3c97a8450f73a9a0
                 }
             }
 
